8b3a48f273bd740f88589ff129c56eeea3c2ba22
SalientArc <nikhildhiman9050@gmail.com>
2022-10-07 11:12:07 +0530

    replaced deprecated method in lua/plugins/bufferline.lua | Shifted colorscheme back to gruvbox

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,8 +53,9 @@ return packer.startup(function(use)
   use "lewis6991/gitsigns.nvim"                       -- Git integration
 
   -- Colorscheme
-  -- use 'sainnhe/gruvbox-material'                   -- Gruvbox with Material Palette
-  use 'navarasu/onedark.nvim'                         -- One Dark Colorscheme
+  use 'sainnhe/gruvbox-material'                   -- Gruvbox with Material Palette
+  -- use 'navarasu/onedark.nvim'                         -- One Dark Colorscheme
+  -- use 'morhetz/gruvbox'
 
   -- Auto-completion
   use "hrsh7th/nvim-cmp"                              -- Completion (cmp) plugin
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,8 @@
--- local colorscheme = "onedark"
+local colorscheme = "gruvbox-material"
 
--- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
--- if not status_ok then
---  vim.notify("colorscheme " .. colorscheme .. " not found!")
---  return
--- end
---
-
-require('onedark').setup {
- -- available themes: dark, darker, warm, warmer, cool, deep
-  style = 'darker'
-}
-require('onedark').load()
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme " not found!")
+  return
+end
